61d68bdbbb90a1414b8d68420392014326c2d480
Klythe <34757795+Klythe@users.noreply.github.com>
2018-06-09 22:17:18 +0000

    added genGsheetDict to pull all gsheet data to memory. also added findGymFromDict to perform checks based on the dict while keeping findGym function intact

--- __main__.py
+++ __main__.py
@@ -10,7 +10,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 
 with open('config.json', 'r') as fd:
-	config = json.load(fd)
+    config = json.load(fd)
 
 mM_raid = re.compile('^Meowth!.*(Coordinate here!)')
 gsheet_creds = ServiceAccountCredentials.from_json_keyfile_name(config['service_account_file'],
@@ -36,9 +36,57 @@ def getGsheetCreds():
         print(err)
         return
     return creds
+    
+def genGsheetDict(gc):
+    for i in range(3):  # three tries to get spreadsheet
+        try:
+            print("opening gsheet")
+            sheet = gc.open_by_key(config['gsheet_id']).sheet1  # connect to google sheet
+        except Exception as err:
+            print("Exception in opening gsheet!")
+            print(err)
+            gc = getGsheetCreds()
+        else:
+            break
+    gyms = [x.lower().strip() for x in sheet.col_values(1)]
+    locations = sheet.col_values(2)
+    
+    return dict(zip(gyms,locations))
+    
+    
+def findGymFromDict(gymDict, details):  # returns a gmaps link if the details are recognized in the cached gsheet info. returns False otherwise
+    if details.lower().strip() in gymDict.keys():
+        try:
+            details = gymDict[details.lower().strip()]
+        except KeyError: # i don't know how we could end up with a keyerror.. but to be safe
+            print("key error found. Returning a false match")
+            return False
+        # stealing Meowth's code to get another maps link
+        if "/maps" in details and "http" in details:
+            mapsindex = details.find('/maps')
+            newlocindex = details.rfind('http', 0, mapsindex)
+            if newlocindex == (- 1):
+                return
+            newlocend = details.find(' ', newlocindex)
+            if newlocend == (- 1):
+                newloc = details[newlocindex:]
+                return newloc
+            else:
+                newloc = details[newlocindex:newlocend + 1]
+                return newloc
+        details_list = details.split()
+        # look for lat/long coordinates in the location details. If provided,
+        # then channel location hints are not needed in the maps query
+        if re.match(r'^\s*-?\d{1,2}\.?\d*,\s+-?\d{1,3}\.?\d*\s*$',
+                    details):  # regex looks for lat/long in the format similar to 42.434546, -83.985195.
+            return "https://www.google.com/maps/search/?api=1&query={0}".format('+'.join(details_list))
 
+        return 'https://www.google.com/maps/search/?api=1&query={0}+{1}'.format('+'.join(details_list), '+'.join(config['loc_list']))
 
-def findGym(gc, details):  # returns a gmaps link if the details are recognized. returns False otherwise
+    else:  # exit if we didn't find a match in the spreadsheet
+        return False
+
+def findGym(gc, details):  # returns a gmaps link if the details are recognized from a direct poll of the gsheet. returns False otherwise
     for i in range(3):  # three tries to get spreadsheet
         try:
             print("opening gsheet")
@@ -105,5 +153,5 @@ async def on_message(message):
 async def on_ready():
     print('Logged in as ' + tinyMeowth.user.name + " with ID " + tinyMeowth.user.id)
 
-
+GsheetDict = genGsheetDict(getGsheetCreds())
 tinyMeowth.run(config['bot_token'])
