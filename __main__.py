163866264173c0ede1e0436111ca1e9ffd06b3f6
tinya <868903+tinya@users.noreply.github.com>
2018-06-13 16:59:03 -0400

    remove floating print

--- __main__.py
+++ __main__.py
@@ -120,8 +120,6 @@ def findGym(raid_name):
     g1 = [x.lower().strip() for x in sheet.get_worksheet(0).col_values(1)]  # Actual gyms names
     g2 = [x.lower().strip() for x in sheet.get_worksheet(1).col_values(1)]  # Gym aliases
 
-    print
-
     if raid_name.lower().strip() in g1:
         details = sheet.get_worksheet(0).cell(g1.index(raid_name.lower().strip()) + 1, 2).value
     elif raid_name.lower().strip() in g2:
